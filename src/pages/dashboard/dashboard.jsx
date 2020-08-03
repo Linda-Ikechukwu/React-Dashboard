9c72c63147be334dd08d6014bc4c8bf4798c60a3
Linda-Ikechukwu <lindaedwin96@gmail.com>
2020-08-03 17:48:52 +0100

    Add Dashboard revenue chart

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 
@@ -7,10 +8,15 @@ import '../../index.scss'
 
 import '../../components/dashboard-cards/dashboard-cards.component'
 import DashboardCards from '../../components/dashboard-cards/dashboard-cards.component';
+import DashboardRevenue from '../../components/dashboard-revenue/dashboard-revenue.component'
 
 const useStyles = makeStyles((theme) => ({
     // necessary for content to be below app bar
     toolbar: theme.mixins.toolbar,
+    margin:{
+        marginTop:'2rem',
+        marginBottom:'2rem',
+    }
 }));
 
 
@@ -22,6 +28,13 @@ const DashboardPage = () => {
         <main className="main-content">
             <div className={classes.toolbar} />
             <DashboardCards/>
+            <Grid container spacing={3} className={classes.margin}>
+                <Grid item sm = {12} md = {4}></Grid>
+                <Grid item sm = {12} md = {8}>
+                   <DashboardRevenue/>
+                </Grid>
+            </Grid>
+                
              
         </main>
     )
